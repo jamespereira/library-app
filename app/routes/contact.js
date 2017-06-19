3a32d78fe5432dd66d1ccb45aec061d5ca10f770
James <james.pereira90@outlook.com>
2017-06-19 21:01:18 +1000

    fix response message and refactor contact to logic in model

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return this.store.createRecord('contact');
+  },
+
+  actions: {
+
+    sendMessage(newContactMessage) {
+      newContactMessage.save().then(() => {
+        this.controller.set('responseMessage', `We got your message and we'll be in touch soon`);
+        this.controller.setProperties({email: '', message: ''});
+      });
+    },
+
+    willTransition() {
+      let model = this.controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+    }
+  }
 });
